21be9516417fca83f9d8dcbf9ac22f76861ecb86
Daniel Portales <dportalesr@gmail.com>
2021-02-03 06:21:02 -0600

    Genesis

---
+++ lib/solidus_yotpo.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'solidus_core'
+require 'solidus_support'
+
+require 'solidus_yotpo/configuration'
+require 'solidus_yotpo/version'
+require 'solidus_yotpo/engine'
+require 'solidus_yotpo/api/client'
+require 'solidus_yotpo/auth'
