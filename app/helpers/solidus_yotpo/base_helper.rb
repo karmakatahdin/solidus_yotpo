e095cbe61ca52db7ea9b3c6f959bb86102b6f8d9
Daniel Portales <dportalesr@gmail.com>
2021-02-22 22:18:11 -0600

    Add helper to generate reviews

---
+++ app/helpers/solidus_yotpo/base_helper.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module SolidusYotpo
+  module BaseHelper
+    def stars(score, editable: false)
+      fragment_name = 'review[score]'
+
+      if score.is_a? SolidusYotpo::Review
+        review = score
+        score = review.score
+        fragment_name = dom_id(review, 'score')
+      end
+
+      (1..5).reverse_each.map do |n|
+        fragment_id = "#{fragment_name.tr('][', '_')}_#{n}".squeeze('_')
+
+        radio_button_tag(fragment_name, n, score.to_i == n, id: fragment_id, disabled: !editable) +
+        label_tag(fragment_id, t("score_#{n}", scope: 'solidus_yotpo.scores'))
+      end.join
+    end
+
+    def score_text(score)
+      score = score.try(:score) || score
+      "#{t('solidus_yotpo.stars', count: score)} #{t('solidus_yotpo.out_of', total: SolidusYotpo.config.max_score)}"
+    end
+  end
+end
