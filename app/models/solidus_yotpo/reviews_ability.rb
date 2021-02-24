888ecae10a6a49fcddc633e31c8b521c5d6582f3
Daniel Portales <dportalesr@gmail.com>
2021-02-24 17:38:48 -0600

    Add ability to detect if product is reviewable by current user; only already purchased products can be reviewed by default

--- app/models/solidus_yotpo/reviews_ability.rb
+++ app/models/solidus_yotpo/reviews_ability.rb
@@ -6,6 +6,9 @@ class SolidusYotpo::ReviewsAbility
   def initialize(user)
     if user.present?
       can :create, SolidusYotpo::Review
+      can :review, Spree::Product do |product|
+        user.orders.joins(:products).complete.exists?(spree_products: { id: product.id })
+      end
     end
   end
 end
